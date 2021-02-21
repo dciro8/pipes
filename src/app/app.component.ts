714803b3d429eb0812b7299cd311d53b92db9fac
David Fernando Ciro Zapata <dciro@intergrupo.com>
2021-02-21 09:17:24 -0500

    PipeLinsAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre: string = `Capitan america`;
+
+  arreglo = [1,2,3,4,5,6,7,8,9];
+  pipe: number =Math.PI;
+
+  porcetaje: number= 0.235;
+
+  moneda: number=1234.5;
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Llego la data');
+    },4500);
+  }
+  );
+
+  heroe={
+    nombre: 'Logan',
+    calve: 'Wolverin',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+  }
 }
